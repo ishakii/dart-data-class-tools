6ecc34d9d213f89f16fb2e32ff4516d289191749
ishak <37158905+ishakii@users.noreply.github.com>
2024-05-18 22:18:58 +0300

    fix copywith not nullable list field

--- src/extension.js
+++ src/extension.js
@@ -1085,15 +1085,22 @@ class DataClassGenerator {
             if (usesValueGetter && p.isNullable) {
                 method += `    ${ clazz.hasNamedConstructor ? `${ p.name }: ` : '' }${ p.name } != null ? ${ p.name }() : this.${ p.name },\n`;
             } 
+
             if (p.isCollection) {
-              method +=
-                `    ${clazz.hasNamedConstructor ? `${p.name}: ` : ""}${
-                  p.name
-                } != null || this.${p.name} != null\n` +
-                `          ? ${p.isList ? "List" : "Map"}.from(${p.name} ?? this.${
-                  p.name
-                } ?? [])\n` +
-                `          : ${p.name} ?? this.${p.name},\n`;
+                if (p.isNullable) {
+                  method +=
+                    `    ${clazz.hasNamedConstructor ? `${p.name}: ` : ""}${
+                      p.name
+                    } != null || this.${p.name} != null\n` +
+                    `          ? ${p.isList ? "List" : "Map"}.from(${
+                      p.name
+                    } ?? this.${p.name} ?? [])\n` +
+                    `          : ${p.name} ?? this.${p.name},\n`;
+                }else {
+                 method +=
+                   `    ${clazz.hasNamedConstructor ? `${p.name}: ` : ""}` +
+                   `List.from(${p.name} ?? this.${p.name}),\n`;
+                }
             } else {
               method += `    ${clazz.hasNamedConstructor ? `${p.name}: ` : ""}${
                 p.name
